468fc4dbd4f41287593713544b44a01ac5aae903
DoJukic <155781308+DoJukic@users.noreply.github.com>
2024-04-10 12:04:25 +0200

    Scroll to top done!

--- wsite/components/scroll_to_top.js
+++ wsite/components/scroll_to_top.js
@@ -3,18 +3,62 @@ class ScrollToTop extends HTMLElement {
         super();
     }
 
+    static targetClass = "sttRotateOnTopDistanceTrigger";
+    
+    static scrollToTopCheckDisplay(element){
+        if (($("body").height() - 200) > $(window).height()) {
+            $(element).css("display", "flex");
+            return;
+        }
+        $(element).css("display", "none");
+    }
+    
+    static scrollToTopCheckRotation(element){
+        if (window.scrollY > 100) {
+            element.classList.remove(ScrollToTop.targetClass)
+            return;
+        }
+        element.classList.add(ScrollToTop.targetClass);
+    }
+
     connectedCallback() {
         this.innerHTML =
-            html`
-            <div class="row" style="justify-content: flex-end; align-items: flex-end;">
+            `
+            <div class="row" style="align-items: flex-end;">
             
-                <img id="scrollToTopImage" class="observerRotateOnNavbar" src="../res/img/scroll_to_top.png"
+                <img class="sttRotateOnTopDistance scrollToTopImage" src="../res/img/scroll_to_top.png"
                     style="width: 40px; height: 40px; pointer-events: auto; opacity: 0.5; cursor: pointer;">
                 <stt-space-maker style="display:block; width:30px;"></stt-space-maker>
             </div>
             <stt-space-maker style="display:block; height:20px;"></stt-space-maker>
         `;
+        
+
+        // If the page is too small there is no use for this button, so we hide it.
+        var me_myself = this;
+        $(window).on('resize', function(){
+            ScrollToTop.scrollToTopCheckDisplay(me_myself);
+        });
+        ScrollToTop.scrollToTopCheckDisplay(me_myself);
+
+        // If we're already at the top, turn us into a scroll-to-bottom button.
+        var targetElement = this.getElementsByClassName("scrollToTopImage")[0];
+        $(window).on('scroll', function(){
+            ScrollToTop.scrollToTopCheckRotation(targetElement);
+        });
+        ScrollToTop.scrollToTopCheckRotation(targetElement);
+
+
+        // The actual functionality is here
+        $(targetElement).on("click", function(){
+            if(!$(targetElement).hasClass(ScrollToTop.targetClass)){
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+            }else{
+                window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+            }
+        })
     }
 }
+
 
 customElements.define('scroll-to-top-component', ScrollToTop);
--- wsite/main_logic.js
+++ wsite/main_logic.js
@@ -1,6 +1,6 @@
 /*
 Lit-html tag override so we don't have to remove them constantly
 Should probably be removed in production, but it's largely harmless
-(jquery's html() is called on a selector, so there is no conflict [tested])
+(jquery's html() is called on a selector, so there should be no conflict)
 */
 function html(s){return s};
