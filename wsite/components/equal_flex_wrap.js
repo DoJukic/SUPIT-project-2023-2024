75da36a042d6e13716bad3111e092e93f3053695
DoJukic <155781308+DoJukic@users.noreply.github.com>
2024-05-26 17:27:39 +0200

    Media queries stink, this is better

--- wsite/components/equal_flex_wrap.js
+++ wsite/components/equal_flex_wrap.js
@@ -4,21 +4,55 @@ class equalFlexWrap extends HTMLElement {
     }
 
     static overflowResizeObserver = null;
-    static rowAttribute = "is-logical-row";
+
+    minElementSizePx = 200;
+    elementGapPx = 20;
 
     connectedCallback() {
+        let temp = ML.getIntAttribute(this, "min-element-size-px");
+        if(Boolean(temp)){
+            this.minElementSizePx = temp;
+        }
+
+        temp = ML.getIntAttribute(this, "element-gap-px");
+        if(Boolean(temp)){
+            this.elementGapPx = temp;
+        }
+        
+        /* We'll want to avoid destroying everything in style while changing things in this case */
+        this.style.setProperty("--splitter-gap", `${this.elementGapPx}px`);
+        this.style.gap = `${this.elementGapPx}px`;
+
         equalFlexWrap.overflowResizeObserver.observe(this);
         this.balanceDisplay();
     }
 
     balanceDisplay() {
-        $(this).children().each(function(){
-            if(ML.getBooleanAttribute(this, equalFlexWrap.rowAttribute)){
-                console.log("AAAAAAAAAAAAAAAAAAAA")
-            }else{
-                console.log("BBBBBBBBBBBBBB")
-            }
-        })
+        let elementSizePx = 0;
+        let childrenAmountPerRow = this.children.length
+
+        let availableWidth = $(this).width();
+
+        let neededSize = childrenAmountPerRow * this.minElementSizePx;
+        let diff = neededSize / availableWidth;
+
+        while(diff > 1){
+            diff = diff / 2;
+            childrenAmountPerRow = childrenAmountPerRow / 2;
+        }
+        
+        childrenAmountPerRow = Math.ceil(childrenAmountPerRow / 2) ;
+        elementSizePx = availableWidth / childrenAmountPerRow - 5;
+
+        
+        console.log("*************************************")
+        console.log(childrenAmountPerRow)
+        console.log(availableWidth)
+        console.log(neededSize)
+        console.log(diff)
+        console.log(elementSizePx)
+        
+        this.style.setProperty("--splitter-basis", `${elementSizePx}px`);
     }
 }
 
